8d561d1de9707a4f74d85402b8562032cbfedb47
DamianGluk <damigluk@gmail.com>
2020-06-13 23:04:31 -0300

    mensaje

--- ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Fixture.java
+++ ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Fixture.java
@@ -7,6 +7,7 @@ import com.example.ourtournament.ListaPartidos;
 import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -26,46 +27,25 @@ public class Fixture extends Fragment {
     FragmentManager AdminFragments;
     FragmentTransaction TransaccionesDeFragment;
 
-    Spinner Jornadas;
-    Spinner LasJornadas;
-    ArrayAdapter<String> Adaptador;
-
-    ArrayList<String> Equipo1;
-    ArrayList<String> Equipo2;
-
-    String Jornada;
-    Button Fixture;
-    private Object AdapterView;
-
+    private Spinner spinner;
+    private static final String[] paths = {"item 1", "item 2", "item 3"};
     @Override
     public View onCreateView(LayoutInflater inflador, @Nullable ViewGroup GrupoDeLaVista, Bundle savedInstanceState) {
-        Log.d("conexion", "entre");
+
         View VistaADevolver;
         VistaADevolver = inflador.inflate(R.layout.fixture, GrupoDeLaVista, false);
         AdminFragments=getFragmentManager();
         MainActivity Principal;
         Principal = (MainActivity) getActivity();
+        ArrayList<String> ListaJornadas = Principal.getJornadas();
 
-        Jornadas = (Spinner) VistaADevolver.findViewById(R.id.Jornadas);
+        spinner = VistaADevolver.findViewById(R.id.Jornadas);
 
-        ArrayList<String> list = new ArrayList<>();
-        list.add("RANJITH");
-        list.add("ARUN");
-        list.add("JEESMON");
-        list.add("NISAM");
-        list.add("SREEJITH");
-        list.add("SANJAY");
-        list.add("AKSHY");
-        list.add("FIROZ");
-        list.add("RAHUL");
-        list.add("ARJUN");
-        list.add("SAVIYO");
-        list.add("VISHNU");
-
-        ArrayAdapter<String> adapter = new ArrayAdapter<String>(android.R.layout.simple_spinner_item, Jornadas);
-        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-        Jornadas.setAdapter(adapter);
-        Jornadas.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+        spinner = VistaADevolver.findViewById(R.id.Jornadas);
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity().getApplicationContext(), android.R.layout.simple_expandable_list_item_1,paths);
+        spinner.setAdapter(adapter);
+        Log.d("conexion", "entre");
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
 
             public void onItemSelected(AdapterView<?> adapterView, View view, int i, long l) {
 
@@ -76,16 +56,6 @@ public class Fixture extends Fragment {
 
             }
         });
-
-        /*
-        Jornadas = VistaADevolver.findViewById(R.layout.Jornadas);
-
-        Jornadas = Principal.getJornadas();
-        LasJornadas = VistaADevolver.findViewById(R.id.Jornadas);
-        Adaptador = Principal.getAdaptador();
-        LasJornadas.setAdapter(Adaptador);
-
-         */
         MostrarListaPartidos();
 
         return VistaADevolver;
