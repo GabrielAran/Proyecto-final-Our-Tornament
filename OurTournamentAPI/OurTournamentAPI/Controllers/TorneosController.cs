4c1dc007e5480915d88f1d5b9ed0a36d10a40e05
DamianGluk <damigluk@gmail.com>
2020-06-19 09:15:31 -0300

    mensaje

---
+++ OurTournamentAPI/OurTournamentAPI/Controllers/TorneosController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OurTournamentAPI.Controllers
+{
+    public class TorneosController : ApiController
+    {
+        [HttpGet]
+        [Route("api/Torneos/{id}")]
+        public IHttpActionResult ObtenerTorneoPorID(int id)
+        {
+            Models.Torneo T = new Models.Torneo();
+            QQSM Conexion = new QQSM();
+            T = Conexion.TraerTorneo(id);
+            if(T!= null)
+            {
+                return Ok(T);
+            }
+            else
+            {
+                return NotFound();
+            }
+            
+        }
+    }
+}
