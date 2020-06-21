f8d017d3e5ce1b9de8dbaff0cf7ee006aa01560b
DamianGluk <damigluk@gmail.com>
2020-06-20 22:12:08 -0300

    mensaje

--- ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Inicio/Inicio.java
+++ ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Inicio/Inicio.java
@@ -11,19 +11,25 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.example.ourtournament.MainActivity;
 import com.example.ourtournament.R;
 
 import java.util.ArrayList;
 
 public class Inicio extends Fragment {
+    Button Noticias,Buscar;
     @Override
     public View onCreateView(LayoutInflater inflador, @Nullable ViewGroup GrupoDeLaVista, Bundle savedInstanceState) {
 
         View VistaADevolver;
         VistaADevolver = inflador.inflate(R.layout.inicio, GrupoDeLaVista, false);
+        Noticias = VistaADevolver.findViewById(R.id.Noticias);
+        Buscar = VistaADevolver.findViewById(R.id.Buscar);
+
 
 
         return VistaADevolver;
