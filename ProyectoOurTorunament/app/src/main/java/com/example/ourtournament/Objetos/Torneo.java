49fcee1e039e5239368a8a76dbc0554b8bb501ba
DamianGluk <damigluk@gmail.com>
2020-06-26 10:34:40 -0300

    mensaje

--- ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Objetos/Torneo.java
+++ ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Objetos/Torneo.java
@@ -6,14 +6,15 @@ public class Torneo
     public String _nombreTorneo;
     public String _contraseniaDeAdministrador;
     public String _linkParaUnirse;
+    public int _TipoDeTorneo;
 
-
-    public Torneo(int idtorneo, String nombreTorneo, String contraseniaDeAdministrador, String linkParaUnirse)
+    public Torneo(int idtorneo, String nombreTorneo, String contraseniaDeAdministrador, String linkParaUnirse,int tipodetorneo)
     {
         _IDTorneo = idtorneo;
         _nombreTorneo= nombreTorneo;
         _contraseniaDeAdministrador = contraseniaDeAdministrador;
         _linkParaUnirse = linkParaUnirse;
+        _TipoDeTorneo = tipodetorneo;
     }
 
     public Torneo()
