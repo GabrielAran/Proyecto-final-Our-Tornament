d6fe671710c8bb695a515e5ec4ae9d4a81756490
GabiLaBestiaProgramable <56962783+GabiLaBestiaProgramable@users.noreply.github.com>
2020-07-02 18:19:51 -0300

    cambios

--- ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Loguearse/CrearCuenta.java
+++ ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Loguearse/CrearCuenta.java
@@ -12,7 +12,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.FrameLayout;
+import android.widget.Toast;
 
 import com.example.ourtournament.MainActivity;
 import com.example.ourtournament.R;
@@ -22,20 +24,34 @@ public class CrearCuenta extends Fragment {
     FragmentTransaction TransaccionesDeFragment;
     Button ConfirmarLogueo;
     SharedPreferences DatosGenerales;
+
+    EditText contra;
+    EditText confContra;
+
     @Override
     public View onCreateView(LayoutInflater inflador, @Nullable ViewGroup GrupoDeLaVista, Bundle savedInstanceState) {
         View VistaADevolver;
         VistaADevolver = inflador.inflate(R.layout.crear_cuenta, GrupoDeLaVista, false);
         AdminFragments=getFragmentManager();
+
         ConfirmarLogueo = VistaADevolver.findViewById(R.id.button);
+        contra = VistaADevolver.findViewById(R.id.contra);
+        confContra = VistaADevolver.findViewById(R.id.confContra);
+
         ConfirmarLogueo.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                final MainActivity Principal = (MainActivity) getActivity();
-                Principal.Entrar();
+                String contraText = contra.getText().toString();
+                String confContraText = confContra.getText().toString();
+                Log.d("Contrasenia", "c1=" + contraText + " c2=" + confContraText);
+                if (!contraText.equals(confContraText)){
+                    Log.d("Contrasenia", "Las contrasenias no coinciden");
+                }else {
+                    final MainActivity Principal = (MainActivity) getActivity();
+                    Principal.Entrar();
+                }
             }
         });
-
         return VistaADevolver;
     }
 }
