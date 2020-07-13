2fb6327bb8b6f8911ff337d356a49f4aeadf002c
DamianGluk <damigluk@gmail.com>
2020-07-13 01:07:55 -0300

    mensaje

--- ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Inicio/AdaptadorListaTorneos.java
+++ ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Inicio/AdaptadorListaTorneos.java
@@ -1,4 +1,49 @@
 package com.example.ourtournament.Inicio;
 
-public class AdaptadorListaTorneos {
+        import android.annotation.SuppressLint;
+        import android.content.Context;
+        import android.util.Log;
+        import android.view.LayoutInflater;
+        import android.view.View;
+        import android.view.ViewGroup;
+        import android.widget.ArrayAdapter;
+        import android.widget.BaseAdapter;
+        import android.widget.TextView;
+
+        import com.example.ourtournament.Objetos.Equipo;
+        import com.example.ourtournament.Objetos.Torneo;
+        import com.example.ourtournament.R;
+
+        import java.util.ArrayList;
+
+public class AdaptadorListaTorneos extends ArrayAdapter<Torneo>
+{
+    private ArrayList<Torneo> _ListaTorneos;
+    private Context _Contexto;
+    private int _Resource;
+
+    public AdaptadorListaTorneos(Context contexto,int Resource,ArrayList<Torneo> ListaTorneos)
+    {
+        super(contexto,Resource,ListaTorneos);
+        this._ListaTorneos = ListaTorneos;
+        this._Contexto = contexto;
+        this._Resource = Resource;
+    }
+
+    @SuppressLint("ViewHolder")
+    public View getView(int pos, View VistaADevolver, ViewGroup GrupoActual)
+    {
+        LayoutInflater MiInflador;
+        if(VistaADevolver == null)
+        {
+            MiInflador = LayoutInflater.from(this._Contexto);
+            VistaADevolver = MiInflador.inflate(_Resource,null);
+        }
+
+
+        Torneo T = getItem(pos);
+
+
+        return  VistaADevolver;
+    }
 }
--- ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Inicio/BuscarTorneos.java
+++ ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/Inicio/BuscarTorneos.java
@@ -5,19 +5,13 @@ import android.app.Fragment;
 import android.app.FragmentManager;
 import android.app.FragmentTransaction;
 import android.os.Bundle;
-import android.util.Log;
-import android.view.KeyEvent;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Button;
 import android.widget.EditText;
 import android.widget.ListView;
-import android.widget.Spinner;
-import android.widget.TextView;
-
 import com.example.ourtournament.MainActivity;
 import com.example.ourtournament.Objetos.Torneo;
 import com.example.ourtournament.R;
@@ -30,25 +24,45 @@ public class BuscarTorneos extends Fragment {
 
     ArrayList<Torneo> ListaTorneos = new ArrayList<>();
     EditText Buscador;
+    ArrayList<Torneo> ListaTorneosSeleccionados = new ArrayList<>();
+    ListView ListaAMostrar;
     @Override
     public View onCreateView(LayoutInflater inflador, @Nullable ViewGroup GrupoDeLaVista, Bundle savedInstanceState) {
-
         View VistaADevolver;
         VistaADevolver = inflador.inflate(R.layout.buscar_torneos, GrupoDeLaVista, false);
         AdminFragments=getFragmentManager();
         LlenarLista();
-        Buscador = VistaADevolver.findViewById(R.id.Buscar);
-        Buscador.setOnEditorActionListener(new EditText.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int i, KeyEvent keyEvent) {
-                if(textView.getText().toString() == "")
-                {
+        final MainActivity Principal = (MainActivity) getActivity();
 
+        ListaAMostrar = VistaADevolver.findViewById(R.id.listaTorneos);
+        AdaptadorListaTorneos Adaptador = new AdaptadorListaTorneos(Principal,R.layout.item_lista_torneos,ListaTorneos);
+        ListaAMostrar.setAdapter(Adaptador);
+        Buscador = VistaADevolver.findViewById(R.id.Buscar);
+        Buscador.addTextChangedListener(new TextWatcher()
+        {
+            public void afterTextChanged(Editable s)
+            {
+            }
+            public void beforeTextChanged(CharSequence s, int start, int count, int after)
+            {
+                AdaptadorListaTorneos Adaptador = new AdaptadorListaTorneos(Principal,R.layout.item_lista_torneos,ListaTorneos);
+                ListaAMostrar.setAdapter(Adaptador);
+            }
+
+            public void onTextChanged(CharSequence s, int start, int before, int count)
+            {
+                ListaTorneosSeleccionados.removeAll(ListaTorneosSeleccionados);
+                for (int a=0;a<ListaTorneos.size();a++)
+                {
+                    if(ListaTorneos.get(a)._nombreTorneo.contains(s))
+                    {
+                        ListaTorneosSeleccionados.add(ListaTorneos.get(a));
+                    }
                 }
-                return false;
+                AdaptadorListaTorneos Adaptador = new AdaptadorListaTorneos(Principal,R.layout.item_lista_torneos,ListaTorneosSeleccionados);
+                ListaAMostrar.setAdapter(Adaptador);
             }
         });
-
 
         return VistaADevolver;
     }
@@ -56,9 +70,10 @@ public class BuscarTorneos extends Fragment {
     public void LlenarLista()
     {
         Torneo T = new Torneo(1,"Copa palermo","lacontra", "aaaaa",2);
+        Torneo A = new Torneo(2,"Gambeta corta","lacontra", "aaaaa",2);
         ListaTorneos.add(T);
+        ListaTorneos.add(A);
         ListaTorneos.add(T);
-        ListaTorneos.add(T);
-        ListaTorneos.add(T);
+        ListaTorneos.add(A);
     }
 }
