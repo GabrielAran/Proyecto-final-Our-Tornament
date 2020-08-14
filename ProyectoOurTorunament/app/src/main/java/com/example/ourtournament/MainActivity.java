1c13236d8a20d7c5b1c6bea050f4402aa2ecb810
damiangluk <damigluk@gmail.com>
2020-08-14 10:14:06 -0300

    Cambio Gluk

--- ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/MainActivity.java
+++ ProyectoOurTorunament/app/src/main/java/com/example/ourtournament/MainActivity.java
@@ -63,7 +63,7 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         AdminFragments=getFragmentManager();
         DatosGenerales = CargarSharedPreferences();
-        //DatosGenerales.EliminarString("contrasenia");
+        DatosGenerales.EliminarString("contrasenia");
         String Nombre = DatosGenerales.ObtenerString("contrasenia","no hay contrasenia");
         if(Nombre=="no hay contrasenia")
         {
