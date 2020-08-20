f11a3fabc5ba5a59457064504e7a5986624162b9
GabiLaBestiaProgramable <56962783+GabiLaBestiaProgramable@users.noreply.github.com>
2020-08-20 19:32:41 -0300

    cambios gabi

--- OurTournamentAPI/OurTournamentAPI/Models/QQSM.cs
+++ OurTournamentAPI/OurTournamentAPI/Models/QQSM.cs
@@ -170,7 +170,7 @@ namespace OurTournamentAPI
                 Tablagoleadores.Add(UnUsuario);
             }
             Desconectar(con);
-            return Tablagoleadores;//fg
+            return Tablagoleadores;
         }
 
         /*
